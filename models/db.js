bccf37d3186d86290113d8937256037a44aa8bc0
revallalokesh <lokeshrevalla3@gmail.com>
2025-07-01 15:06:29 +0530

    feat: connect to Railway DB and fix deployment

--- models/db.js
+++ models/db.js
@@ -1,12 +1,13 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const pool = mysql.createPool({
+export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASS
-}).promise();
-
-export default pool;
+  port: process.env.DB_PORT || 3306, // default MySQL port
+  waitForConnections: true,
+  connectionLimit: 10,
+});
